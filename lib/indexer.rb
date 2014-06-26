c1967f4c1abfee8266ac66770a5099950c3edd0a
Nathanael Burt <nathanael.burt@gmail.com>
2014-06-26 13:28:02 -0600

    refactored out conditional statements

--- lib/indexer.rb
+++ lib/indexer.rb
@@ -7,14 +7,7 @@ class Indexer
   def by_id
     indexed_by_id = {}
     @amusement_parks.each do |park|
-      park_hash = {
-        :id => park[:id],
-        :name => park[:name],
-        :city => park[:city],
-        :state => park[:state],
-        :country => park[:country]
-      }
-      indexed_by_id[park[:id]] = park_hash
+      indexed_by_id[park[:id]] = park
     end
     indexed_by_id
   end
@@ -22,20 +15,8 @@ class Indexer
   def by_country
     indexed_by_country = {}
     @amusement_parks.each do |park|
-      country_parks = []
-      park_hash = {
-        :id => park[:id],
-        :name => park[:name],
-        :city => park[:city],
-        :state => park[:state],
-        :country => park[:country]
-      }
-      if indexed_by_country[park[:country]]
-        indexed_by_country[park[:country]] << park_hash
-      else
-        country_parks << park_hash
-        indexed_by_country[park[:country]] = country_parks
-      end
+      indexed_by_country[park[:country]] ||= []
+      indexed_by_country[park[:country]] << park
     end
     indexed_by_country
   end
@@ -43,21 +24,9 @@ class Indexer
   def by_country_and_state
     indexed_by_country_state = {}
     @amusement_parks.each do |park|
-      country_state_parks = []
-      park_hash = {
-        :id => park[:id],
-        :name => park[:name],
-        :city => park[:city],
-        :state => park[:state],
-        :country => park[:country]
-      }
       state_country_string = create_state_country_string(park[:state], park[:country])
-      if indexed_by_country_state[state_country_string]
-        indexed_by_country_state[state_country_string] << park_hash
-      else
-        country_state_parks << park_hash
-        indexed_by_country_state[state_country_string] = country_state_parks
-      end
+      indexed_by_country_state[state_country_string] ||= []
+      indexed_by_country_state[state_country_string] << park
     end
     indexed_by_country_state
   end
